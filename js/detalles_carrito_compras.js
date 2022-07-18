4849aec3eb4352adc1487acb50a355f515025544
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-07-18 02:59:18 -0500

    cambios

--- js/detalles_carrito_compras.js
+++ js/detalles_carrito_compras.js
@@ -1,17 +1,11 @@
-//desplegar carrito compras
-/*
-$(document).on('click', '#icono-carrito-compras-main', () =>{        
-    $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito"); 
-  });
-  */
-
   //FUNCION para desplegar los detalles del carrito
 
 $(document).on('click', '.btn-ver' , function(){                
     //despliegue detalles con toggle          
     $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito");        
-});       
-         
+});   
+
+
 $(document).on('click', '.div-detalles-carrito', () =>{        
     $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito"); 
 });
@@ -20,4 +14,8 @@ $(document).on('click', '.icono-carrito-compras', () =>{
   $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito"); 
 });
 
+$(document).on('click', '.btn-ordenar', () =>{  
+  $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito"); 
+});
+  
 
