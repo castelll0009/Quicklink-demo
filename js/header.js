089d46597f308c23a968cc7e5ae1b3e56bcf0d3d
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-07-19 16:05:04 -0500

    al fin morida

--- js/header.js
+++ js/header.js
@@ -4,6 +4,6 @@ let img_logo_mordido = "imgs/logo-quicklink-mordido.png";
 document.querySelector("#id-cont-imagen-logo").addEventListener("click",function(){
  $("#id-imagen-logo").attr("src",img_logo_mordido);
     setTimeout(function(){
-        $("#id-imagen-logo").attr("src","../imgs/img_logo");
+        $("#id-imagen-logo").attr("src",img_logo_mordido);
     },3000);    
 });
