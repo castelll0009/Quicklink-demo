0673862e544878d3dddf99a6bc2aa9eb849761f2
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-07-19 15:31:54 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 document.querySelector("#id-cont-imagen-logo").addEventListener("click",function(){
- $("#id-imagen-logo").attr("src","imgs/logo-quicklink-mordido.png");
+ $("#id-imagen-logo").attr('src',"imgs/logo-quicklink-mordido.png");
     setTimeout(function(){
-        $("#id-imagen-logo").attr("src","imgs/logo-quicklink.png");
+        $("#id-imagen-logo").attr('src',"imgs/logo-quicklink.png");
     },3000);    
 });
