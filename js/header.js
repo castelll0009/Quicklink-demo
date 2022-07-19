fbc0b7003fbedf06a4cc7d23e1a8419fa14332ca
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-07-19 15:34:54 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,9 @@
+$(".img-detalles-producto").attr("src",src_imagen);    
+let img_logo = "logo-quicklink.png";
+let img_logo_mordido = "logo-quicklink-mordido.png";
 document.querySelector("#id-cont-imagen-logo").addEventListener("click",function(){
- $("#id-imagen-logo").attr('src',"imgs/logo-quicklink-mordido.png");
+ $("#id-imagen-logo").attr("src",img_logo);
     setTimeout(function(){
-        $("#id-imagen-logo").attr('src',"imgs/logo-quicklink.png");
+        $("#id-imagen-logo").attr("src",img_logo_mordido);
     },3000);    
 });
