e1d75610f0c1fde3e42f1da3709fd37fd37c93f9
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-07-19 15:26:43 -0500

    mordido

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 document.querySelector("#id-cont-imagen-logo").addEventListener("click",function(){
- $("#id-imagen-logo").attr("src","../imgs/logo-quicklink-mordido.png");
+ $("#id-imagen-logo").attr("src","/imgs/logo-quicklink-mordido.png");
     setTimeout(function(){
         $("#id-imagen-logo").attr("src","/imgs/logo-quicklink.png");
     },3000);    
